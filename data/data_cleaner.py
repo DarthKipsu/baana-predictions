124cff436ae2582fa3bf785c78bcb12fe7022f5d
Verna Koskinen <darth.kipsu@gmail.com>
2016-01-29 09:40:04 +0200

    add wroking version for saving labels to file

--- data/data_cleaner.py
+++ data/data_cleaner.py
@@ -27,27 +27,45 @@ def write_to_file(path, data):
     f.close()
 
 def clean_labels():
+    """
+    Reades labels from cyclics count files and writes them on a file without all the unneeded extra
+    information.
+    """
+    # Read data from 1.1.2013 until 28.4.2015
     sheet = read_xlsx_file('original/Baanan_pyorailijamaarat.xlsx', 'Taul1')
-    data = [[row[0].value, row[2].value] for row in sheet.iter_rows('B3:D50')] # row0 value for making sure data is correct
-    print(data)
-    return [[row[2].value] for row in sheet.iter_rows('B3:D850')]
+    data = [[row[2].value] for row in sheet.iter_rows('B3:D850')]
 
-def print_content(path, sheet_name, cells):
-    sheet = read_xlsx_file(path, sheet_name)
-    for row in sheet.iter_rows(cells):
-        # row[0] = datetime object for the count
-        # row[1] = the day of week for the count
-        # row[2] = the actual cyclist count
-        print(row[0].value, row[1].value, row[2].value)
+    # Read data between 29.4.2015 and 30.9.2015
+    with open('original/Helsingin_pyorailijamaarat.csv') as csv_file:
+        csv_data = [row.split(',') for row in csv_file.readlines()]
+        hour = 0
+        count = 0
+        for i in range(11593, 15313):
+            count += int(csv_data[i][16])
+            hour += 1
+            if (hour == 24):
+                data.append([count])
+                hour = 0
+                count = 0
+
+    # Read data between 1.10.2015 and 31.12.2015
+    sheet = read_xlsx_file('original/Baana_syys_joulukuu.xlsx', 'Pohjoinen Rautatiekatu')
+    hour = 0
+    count = 0
+    for row in sheet.iter_rows('A2:B2209'):
+        count += row[1].value
+        hour += 1
+        if (hour == 24):
+            data.append([count])
+            hour = 0
+            count = 0
+    return data
 
 def print_xml(path):
     soup = read_xml_file(path)
     for feature in soup.find_all('MeasurementTimeseries'):
         # prints feature ids
         print(feature.get('gml:id'))
-
-# currently the data is in rows B3:D850
-print_content('original/Baanan_pyorailijamaarat.xlsx', 'Taul1', 'B3:D50')
 
 print_xml('original/fmi-2013-2014.xml')
 
