6d3ad49022d7a9d7bb72e00fc89b7d77e5af4945
Verna Koskinen <darth.kipsu@gmail.com>
2016-02-18 16:41:37 +0200

    allow getting data with day of the week included

--- ml/load_data.py
+++ ml/load_data.py
@@ -11,6 +11,24 @@ def read_cyclist_data():
 def rain_temp_snow():
     return read_weather_data()[:,[0,1,2]]
 
+def add_day_of_week(data):
+    '''
+    Monday = 0, Tuesday = 1, ..., Sunday = 6
+    '''
+    di = len(data[0])
+    with_days = np.zeros((len(data), di+1))
+    with_days[:,:-1] = data
+    for i in range(6):
+        days = with_days[i::7]
+        days[:,[di]] = i+1
+    return with_days
+
+def rain_temp_snow_days():
+    return add_day_of_week(read_weather_data()[:,[0,1,2]])
+
+def cyclist_days():
+    return add_day_of_week(read_cyclist_data())
+
 def cycklists_by_hundreds():
     data = read_cyclist_data()
     return data / 100
